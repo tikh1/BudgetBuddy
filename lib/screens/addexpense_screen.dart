e6a53b78ebe79204baee2c651dbc306f3424f147
Yunus Eğriboz <yunus.egriboz@gmail.com>
2024-12-12 01:30:15 +0300

    feat: add expense feature added

--- lib/screens/addexpense_screen.dart
+++ lib/screens/addexpense_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import '../services/api.dart';
 
 class AddExpense extends StatefulWidget {
   @override
@@ -12,17 +14,63 @@ class _AddExpenseState extends State<AddExpense> {
   final _titleController = TextEditingController();
   final _descriptionController = TextEditingController();
   final _amountController = TextEditingController();
+  bool _isLoading = false;
+  String _errorMessage = '';
 
-  Future<void> _saveExpense() async {
-    final prefs = await SharedPreferences.getInstance();
-    final expenses = prefs.getStringList('expenses') ?? [];
+  Future<void> _addExpense() async {
+    final title = _titleController.text;
+    final description = _descriptionController.text;
+    final amount = _amountController.text;
 
-    expenses.add(
-      '${_titleController.text}|${_descriptionController.text}|${_amountController.text}',
-    );
+    if (title.isEmpty || description.isEmpty || amount.isEmpty) {
+      setState(() {
+        _errorMessage = 'Lütfen tüm alanları doldurun';
+      });
+      return;
+    }
 
-    await prefs.setStringList('expenses', expenses);
-    Navigator.pop(context);
+    setState(() {
+      _isLoading = true;
+      _errorMessage = '';
+    });
+
+    final url = API_BASE + API_EXPENDITURES; // API URL'nizi buraya ekleyin
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'title': title,
+          'description': description,
+          'amount': amount ,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+        if (responseData['success'] == true) {
+          final prefs = await SharedPreferences.getInstance();
+          await prefs.setString('token', responseData['data']['token']);
+          context.go('/home');
+        } else {
+          setState(() {
+            _errorMessage = 'Kayıt başarısız. Lütfen tekrar deneyin.';
+          });
+        }
+      } else {
+        setState(() {
+          _errorMessage = 'Kayıt başarısız. Lütfen bilgilerinizi kontrol edin.';
+        });
+      }
+    } catch (error) {
+      setState(() {
+        _errorMessage = 'Bir hata oluştu. Lütfen tekrar deneyin.';
+      });
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
   }
 
   @override
@@ -50,8 +98,10 @@ class _AddExpenseState extends State<AddExpense> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _saveExpense,
-              child: Text('Onayla'),
+              onPressed: _isLoading ? null : _addExpense,
+              child: _isLoading
+                  ? CircularProgressIndicator(color: Colors.white)
+                  : Text('Onayla'),
             ),
           ],
         ),
