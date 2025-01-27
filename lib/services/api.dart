545d5bc186cd334e46f04188b3f84dcc33359a5b
Yunus Eğriboz <egribozyunus@gmail.com>
2025-01-28 00:37:25 +0300

    API_BASE domain changed

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,4 +1,4 @@
-const String API_BASE = 'https://budgetbuddy.glosoft.net/';
+const String API_BASE = 'https://budgetbuddy.turkani.me/';
 const String API_LOGIN = 'api/login';
 const String API_REGISTER = 'api/register';
 const String API_EXPENDITURES = 'api/expenditures';
