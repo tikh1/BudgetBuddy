aec331a4535323815f99bf5970caeb1164049646
atakanseventyfive <atakanseventyfive@gmail.com>
2025-01-29 22:08:01 +0000

    added data in api_response model

--- lib/models/api_response.dart
+++ lib/models/api_response.dart
@@ -1,6 +1,7 @@
 class ApiResponse {
   final bool success;
   final String? error;
+  final List<dynamic>? data;
 
-  ApiResponse({required this.success, this.error});
+  ApiResponse({required this.success, this.error, this.data});
 }
