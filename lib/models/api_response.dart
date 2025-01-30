75db94902ad6ba091c775e1edaacbbe2fb91e5b2
Yunus Eğriboz <egribozyunus@gmail.com>
2025-01-30 23:33:40 +0300

    minor changes

--- lib/models/api_response.dart
+++ lib/models/api_response.dart
@@ -1,9 +1,9 @@
 class ApiResponse {
   final bool success;
   final String? error;
-  final Map<String, dynamic>? data;
+  final List<dynamic>? data;
 
   ApiResponse({required this.success, this.error, this.data});
 
-  String? get username => data?['user']?['name'];
+  get username => null;
 }
