daca316cd8dc0b6e761242afe6fae51a2ed0a1f9
atakanseventyfive <atakanseventyfive@gmail.com>
2025-01-30 19:57:40 +0000

    new animation.json uploaded

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -17,6 +17,7 @@ class LoadingScreen extends StatelessWidget {
           onLoaded: (composition) {
             Future.delayed(
               const Duration(seconds: 3),
+              // ignore: use_build_context_synchronously
               () => context.go('/home'),
             );
           },
